660a36ba88b10e253504280f887f663338fc2fec
arturo <arturo.x@live.com>
2025-05-10 13:36:46 -0400

    update config app

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCPGLjXnpsF_ofsbzYXJt2CxOZFevFIY8Y",
-    authDomain: "built-by-rain.firebaseapp.com",
-    projectId: "built-by-rain",
-    storageBucket: "built-by-rain.appspot.com",
-    messagingSenderId: "1028711282275",
-    appId: "1:1028711282275:web:0792a5e148d68bc8266a56"
-  };
-  
-  export default firebaseConfig;
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+export default firebaseConfig;
